0e8fa15527723aa3d63d4dfd121500d940e1bfdc
Zalan Meggyesi <zalanmeggyesi@yahoo.com>
2013-02-18 22:56:45 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup, Command
+
+# run our tests
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys, subprocess
+        errno = subprocess.call([sys.executable, 'recordtype.py'])
+        raise SystemExit(errno)
+
+
+setup(name='recordtype',
+      version='1.2',
+      url='https://bitbucket.org/ericvsmith/recordtype',
+      author='Eric V. Smith',
+      author_email='eric@trueblade.com',
+      description='Similar to namedtuple, but instances are mutable.',
+      long_description=open('README.txt').read() + '\n' + open('CHANGES.txt').read(),
+      classifiers=['Development Status :: 5 - Production/Stable',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Apache Software License',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Programming Language :: Python :: 3.2',
+                   ],
+      license='Apache License Version 2.0',
+      py_modules=['recordtype'],
+
+      cmdclass = {'test': PyTest},
+      )
